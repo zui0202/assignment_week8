fcdb77ad86bc3e6e4c797691822d1031a27bd594
zui0202 <65958007+zui0202@users.noreply.github.com>
2023-04-20 18:41:18 +0900

    Create arrange.sh

---
+++ arrange.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mv $(find ./files -type f | grep -i '^./files/A') a/.
+mv $(find ./files -type f | grep -i '^./files/B') b/.
+mv $(find ./files -type f | grep -i '^./files/C') c/.
+mv $(find ./files -type f | grep -i '^./files/D') d/. 
+mv $(find ./files -type f | grep -i '^./files/E') e/.
+mv $(find ./files -type f | grep -i '^./files/F') f/.
+mv $(find ./files -type f | grep -i '^./files/G') g/. 
+mv $(find ./files -type f | grep -i '^./files/H') h/. 
+mv $(find ./files -type f | grep -i '^./files/I') i/. 
+mv $(find ./files -type f | grep -i '^./files/J') j/. 
+mv $(find ./files -type f | grep -i '^./files/K') k/. 
+mv $(find ./files -type f | grep -i '^./files/L') l/. 
+mv $(find ./files -type f | grep -i '^./files/M') m/. 
+mv $(find ./files -type f | grep -i '^./files/N') n/. 
+mv $(find ./files -type f | grep -i '^./files/O') o/. 
+mv $(find ./files -type f | grep -i '^./files/P') p/. 
+mv $(find ./files -type f | grep -i '^./files/Q') q/. 
+mv $(find ./files -type f | grep -i '^./files/R') r/. 
+mv $(find ./files -type f | grep -i '^./files/S') s/. 
+mv $(find ./files -type f | grep -i '^./files/T') t/. 
+mv $(find ./files -type f | grep -i '^./files/U') u/. 
+mv $(find ./files -type f | grep -i '^./files/V') v/. 
+mv $(find ./files -type f | grep -i '^./files/W') w/. 
+mv $(find ./files -type f | grep -i '^./files/X') x/. 
+mv $(find ./files -type f | grep -i '^./files/Y') y/. 
+mv $(find ./files -type f | grep -i '^./files/Z') z/.  
